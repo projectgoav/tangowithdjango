e5f43dc99ea588acb9afa4b265f47d833683159b
projectgoav <2025797M@student.gla.ac.uk>
2015-01-17 21:47:53 +0000

    completed base chapter 6 stuff + updated .gitignore again

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+#Category Class
+class Category(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+
+	def __unicode__(self):
+		return self.name
+
+
+#Page Class
+class Page(models.Model):
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
+
+	def __unicode__(self):
+		return self.title
