014f17c4fc648a3b8effbc1b778d74db3568f376
projectgoav <2025797M@student.gla.ac.uk>
2015-01-26 20:55:17 +0000

    Chapter 10 - completed

--- rango/views.py
+++ rango/views.py
@@ -199,7 +199,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {'joke' : "Why was the scarecrow promoted? Because he was out standing in his field!"})
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
 @login_required
