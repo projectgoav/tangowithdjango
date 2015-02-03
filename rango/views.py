97e210b65ca3e3d718bd386492f625362f4c4d30
projectgoav <2025797M@student.gla.ac.uk>
2015-02-03 19:33:20 +0000

    Chapter 11 - completed

--- rango/views.py
+++ rango/views.py
@@ -4,12 +4,14 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 # Create your views here.
 from django.http import HttpResponse
 
 #Basic Index
 def index(request):
+
     #Queries for all categories by likes and then orders and shows top 5
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
@@ -17,16 +19,47 @@ def index(request):
                     'pages': page_list,
                     }
 
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context_dict)
+
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            visits = visits + 1
+            # ...and update the last visit cookie, too.
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+
+    response = render(request,'rango/index.html', context_dict)
+
+    return response
 
 #Basic About
 def about(request):
+    if request.session.get('visits'):
+        count= request.session.get('visits')
+    else:
+        count = "Unknown"
 
-	#Adds the about Info to the page
-	context_dic = { 'Page_Content' : "This tutorial has been put together by Ewan McCartney (2025797M)"}
-
-	return render(request, 'rango/about.html', context_dic)
+    #Adds the about Info to the page
+    context_dic = { 'Page_Content' : 'This tutorial has been put together by Ewan McCartney (2025797M)',
+                    'visits' : count }
+                    
+    return render(request, 'rango/about.html', context_dic)
 
 def category(request, category_name_slug):
 
